ef9bb7b5604ce085f1aed27856aebe6155d3c8ff
dxj728 <dxj728@163.com>
2019-03-26 00:22:17 +0800

    add: 多进程练习

--- multiprocessing_test.py
+++ multiprocessing_test.py
@@ -4,5 +4,27 @@
 # Time: 2019年03月26日00时
 # 说明：多进程练习（廖雪峰的python教程：多进程）
 
+# 进程池(Pool)
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name,j):
+	print('Run task %s (%s)...' % (name, os.getpid()))
+	start = time.time()
+	time.sleep(random.random() * 3)
+	end = time.time()
+	print(j)
+	print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+if __name__=='__main__':
+	print('Parent process %s.' % os.getpid())
+	j=0
+	p = Pool(4)				# 同时运行的最多进程数，实际效果与CPU核心数有关，
+	for i in range(5):
+		p.apply_async(long_time_task, args=(i, j))
+	print('Waiting for all subprocesses done...')
+	p.close()
+	p.join()
+	print('All subprocesses done.')
 
 
