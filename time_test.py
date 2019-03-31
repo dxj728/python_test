6461b4e115a0603f9d1a5edf8b4ec3881280c659
dxj728 <dxj728@163.com>
2019-04-01 00:26:13 +0800

    add:增加了时间类模块的练习

--- time_test.py
+++ time_test.py
@@ -13,7 +13,7 @@ utc_now = datetime.datetime.utcnow()
 # 返回当前的时间，返回值类型：datetime类型，精确至微秒 例：2019-03-31 16:56:02.049465
 dt_today = datetime.datetime.now()
 days = dt_today.day		# 返回这个月当天的天数，int类型，例如：31
-day = dt_today.isoweekday()		# 返回现在的月份，int类型，例：3
+day = dt_today.isoweekday()		# 返回int类型1~7 代表周一~周日
 
 # 将当前的datetime类型时间转换为可读字符串，返回值类型：string字符串
 str_now1 = dt_today.strftime('%Y/%m/%d %H:%M:%S %p')		# 例：2019/03/31 17:49:00 PM
@@ -33,11 +33,11 @@ dt1 = datetime.datetime(year=1994, month=7, day=28, hour=12, minute=12, second=1
 dt2 = datetime.datetime(1995, 1, 29, 6, 6, 6)
 
 # 将一个时间区间转换为特定的时间格式，返回值类型：timedelta类型，例：1 day, 0:00:00
-# time或datetime相减后值均为timedelta类型
 delta = datetime.timedelta(days=1)
 
 
 class old_day(object):
+	# 初始化方法：确定当前时刻
 	def __init__(self):
 		self.today = datetime.datetime.now()
 
@@ -45,11 +45,27 @@ class old_day(object):
 	def get_one_day_ago(self):
 		oneday = datetime.timedelta(days=1)
 		day = self.today - oneday
-		date_one_day_age = datetime.datetime(day.year, day.month, day.day)
-		print(date_one_day_age.strftime('%Y/%m/%d'))
-		return date_one_day_age
+		date_one_day_ago = datetime.datetime(day.year, day.month, day.day)
+		print(date_one_day_ago.strftime('%Y/%m/%d'))
+		return date_one_day_ago
 
-	# 返回前一天的开始时刻和结束时刻
+	# 返回前一周的时刻,返回值类型：datetime（默认为00时刻）
+	def get_one_week_ago(self):
+		one_week = datetime.timedelta(days=7)
+		day = self.today - one_week
+		date_one_week_ago = datetime.datetime(day.year, day.month, day.day, 0, 0, 0)
+		print(date_one_week_ago.strftime('%Y/%m/%d'))
+		return date_one_week_ago
+
+	# 返回前一月（30天）的时刻,返回值类型：datetime（默认为00时刻）
+	def get_one_month_ago(self):
+		one_month = datetime.timedelta(days=30)
+		day = self.today - one_month
+		date_one_month_ago = datetime.datetime(day.year, day.month, day.day, 0, 0, 0)
+		print(date_one_month_ago.strftime('%Y/%m/%d'))
+		return date_one_month_ago
+
+	# 返回前一天的开始时刻（datetime类型）、结束时刻（datetime类型）
 	def get_last_day(self):
 		oneday = datetime.timedelta(days=1)
 		day = self.today - oneday
@@ -60,32 +76,33 @@ class old_day(object):
 		print('--'.join([str_last_day_start, str_last_day_end]))
 		return last_day_start, last_day_end
 
-	def week_get(self):
+	# 返回前一周的开始时刻（datetime类型）、结束时刻（datetime类型）
+	def get_last_week(self):
 		dayscount = datetime.timedelta(days=self.today.isoweekday())
-		dayto = self.today - dayscount
+		last_week_end = self.today - dayscount		# 计算出上周的截止时间
 		sixdays = datetime.timedelta(days=6)
-		dayfrom = dayto - sixdays
-		date_from = datetime.datetime(dayfrom.year, dayfrom.month, dayfrom.day, 0, 0, 0)
-		date_to = datetime.datetime(dayto.year, dayto.month, dayto.day, 23, 59, 59)
+		last_week_start = last_week_end - sixdays		# 计算出上周的开始时间
+		date_from = datetime.datetime(last_week_start.year, last_week_start.month, last_week_start.day, 0, 0, 0)
+		date_to = datetime.datetime(last_week_end.year, last_week_end.month, last_week_end.day, 23, 59, 59)
 		print('---'.join([str(date_from), str(date_to)]))
+		return last_week_start, last_week_end
 
-	def month_get(self):
-		"""
-		返回上个月第一个天和最后一天的日期时间
-		:return
-		date_from: 2016-01-01 00:00:00
-		date_to: 2016-01-31 23:59:59
-		"""
-		dayscount = datetime.timedelta(days=self.today.day)
-		dayto = self.today - dayscount
-		date_from = datetime.datetime(dayto.year, dayto.month, 1, 0, 0, 0)
-		date_to = datetime.datetime(dayto.year, dayto.month, dayto.day, 23, 59, 59)
-		print('---'.join([str(date_from), str(date_to)]))
-		return date_from, date_to
+	# 返回前一个月第一天和最后一天的日期时间（均为datetime类型）
+	def get_last_month(self):
+		dayscount = datetime.timedelta(days=self.today.day)		# 获取今天在本月的天数
+		last_month_end = self.today - dayscount		# 获取上个月的截止时间
+		last_month_start = datetime.datetime(last_month_end.year, last_month_end.month, 1, 0, 0, 0)
+		last_month_end = datetime.datetime(last_month_end.year, last_month_end.month, last_month_end.day, 23, 59, 59)
+		print('---'.join([str(last_month_start), str(last_month_end)]))
+		return last_month_start, last_month_end
 
 
 if __name__ == '__main__':
 	old_day = old_day()
 	old_day.get_one_day_ago()
-	old_day.week_get()
-	old_day.month_get()
+	old_day.get_one_week_ago()
+	old_day.get_one_month_ago()
+	old_day.get_last_day()
+	old_day.get_last_week()
+	old_day.get_last_month()
+
