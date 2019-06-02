7bfff74b00511023573a3f3ec6c34b56d7177571
dxj728 <dxj728@163.com>
2019-06-03 07:33:59 +0800

    add:word文档传输

--- selenium_test.py
+++ selenium_test.py
@@ -17,21 +17,56 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+import time
 
 # 打开百度进行搜索
 browser = webdriver.Chrome()
 try:
-	browser.get('http://www.baidu.com')
+	browser.get('https://www.baidu.com')
 	input = browser.find_element_by_id('kw')
 	input.send_keys('python')
 	input.send_keys(Keys.ENTER)
-	wait = WebDriverWait(browser, 10)
+	wait = WebDriverWait(browser, 5)
 	wait.until(EC.presence_of_all_elements_located((By.ID, 'content_left')))
-	print(browser.current_url)			# 当前页面的url
-	print(browser.get_cookies())		# 当前的cookies
-	print(browser.page_source)			# 当前界面的源代码
+	time.sleep(3)
+	input.clear()
+	time.sleep(1)
+	input.send_keys('hello world!')
+	input.send_keys(Keys.ENTER)
+	wait = WebDriverWait(browser, 5)
+	wait.until(EC.presence_of_all_elements_located((By.ID, 'content_left')))
+	browser.back()
+	browser.execute_script('window.open()')
+	print(browser.window_handles)
+	browser.switch_to.window(browser.window_handles[1])
+	browser.get('https://www.github.com')
+	wait = WebDriverWait(browser, 5)
+	browser.switch_to.window(browser.window_handles[0])
+	time.sleep(1)
+	browser.back()
+	time.sleep(3)
+	browser.forward()
+	print('over')
 finally:
-	browser.close()
+	browser.close()		#todo: 此处未能全部关闭，有待解决
+
+#
+# from selenium import webdriver
+# browser = webdriver.Chrome()
+# browser.get('https://www.baidu.com')
+# input_first = browser. find_element_by_id('kw')
+# print(input_first)
+# browser.close()
 
 
-
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium .webdriver.support import expected_conditions as EC
+# browser = webdriver.Chrome()
+# browser.get('https://www.baidu.com/')
+# wait = WebDriverWait(browser,10)
+# input = wait.until(EC.presence_of_element_located((By.ID,'kw')))
+# print(input)
+# print("over")
+# browser.close()
---
+++ test.py
@@ -0,0 +1,5 @@
+# --coding:utf-8--
+# File: test.py
+# Author: dxj728
+# Time: 2019年06月03日06时
+# 说明：
