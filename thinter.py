741781ca8215e3d23634efcdd9efacdf767d6320
dxj728 <dxj728@163.com>
2019-07-10 23:49:53 +0800

    add:图形化编程练习

---
+++ thinter.py
@@ -0,0 +1,18 @@
+# --coding=utf-8--
+# File: thinter.py
+# Author: dxj728
+# Time: 2019年07月04日23时
+# 说明：python图形化编程库thinter的使用
+
+import _tkinter
+import tkinter
+import tkinter.messagebox
+
+root = tkinter.Tk()
+
+root.title('猜数游戏--by dxj728')
+root.geometry('280x80+400+300')
+
+root.mainloop()
+
+
