6a9fd1e8413ff50b0c870e3644b21bb239d33f46
dxj728 <dxj728@163.com>
2019-08-21 23:57:05 +0800

    add:os模块学习

--- os_test.py
+++ os_test.py
@@ -2,7 +2,22 @@
 # File: os_test.py
 # Author: dxj728
 # Time: 2019年08月19日23时
-# 说明：os模块，os.path模块学习笔记
+# 说明：python3.6下 os模块，os.path模块学习笔记
 
+import os
 
+## *********************** os 模块 ***********************************
+
+x = os.getcwd()			# 获取当前所在路径，返回值: 字符串
+
+dir_path = r'D:\test'
+x = os.listdir(dir_path)		# 获取指定路径下文件和目录名称的列表list，返回值: list[string,]
+
+os.chdir(dir_path)			# 将当前工作目录改为指定目录，无返回值（在任何情况下都返回None）
+
+dir_path = r'D:\test\xixi'
+os.mkdir(dir_path)		# 创建一个目录，并指定访问权限（8进制，默认权限511，linux下777权限，windows下无mode参数可忽略），无返回值，目录存在是报WindowsError错误
+
+dir_paths = r'D:\test\haha\so\e'
+os.makedirs(dir_paths)		# 递归创建目录，并指定访问权限（windows下mode参数无用），无返回值，目录存在是报错
 
