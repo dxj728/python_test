2848c8804b97e6671ca48d0b79c8b4173716b1c4
dxj728 <dxj728@163.com>
2019-12-07 00:48:21 +0800

    add:threading学习

--- threading_test.py
+++ threading_test.py
@@ -14,4 +14,17 @@ import os
 # else:
 # 	print('主线程：%s 子线程：%s'% (os.getpid(),pid))
 
+import threading
+
+def action(max):
+	for i in range(max):
+		print(threading.current_thread().name + ':' + str(i))
+
+threading.Thread(target=action, args=(100,), name="新线程").start()
+for i in range(100):
+	if i == 20:
+		jt = threading.Thread(target=action, args=(100,), name ="被join的线程")
+		jt.start()
+		jt.join()
+	print(threading.current_thread().name + ' ' + str(i))
 
