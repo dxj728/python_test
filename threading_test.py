2bb701a0c1c006a6f3e7272f7acdc742a040c48b
dxj728 <dxj728@163.com>
2019-12-25 00:59:59 +0800

    add:线程池test

--- threading_test.py
+++ threading_test.py
@@ -32,23 +32,54 @@ import os
 # 	print(threading.current_thread().name + ' ' + str(i))
 
 import threading
-'''继承Thread类来创建线程类------------'''
-class FkThread(threading.Thread):
-	def __init__(self):
-		threading.Thread.__init__(self)
-		self.i = 0
+# '''继承Thread类来创建线程类------------'''
+# class FkThread(threading.Thread):
+# 	def __init__(self):
+# 		threading.Thread.__init__(self)
+# 		self.i = 0
+#
+# 	## 重写run方法作为线程执行体
+# 	def run(self):
+# 		while self.i < 100:
+# 			print(threading.current_thread().getName() + ': ' + str(self.i))
+# 			self.i += 1
+#
+# for i in range(100):
+# 	print(threading.current_thread().getName() + ': ' + str(i))
+# 	if i == 20:
+# 		ft1 = FkThread()
+# 		ft1.start()
+# 		ft2 = FkThread()
+# 		ft2.start()
+# print("执行完成")
 
-	## 重写run方法作为线程执行体
-	def run(self):
-		while self.i < 100:
-			print(threading.current_thread().getName() + ': ' + str(self.i))
-			self.i += 1
+# ***************线程池**********************
 
-for i in range(100):
-	print(threading.current_thread().getName() + ': ' + str(i))
-	if i == 20:
-		ft1 = FkThread()
-		ft1.start()
-		ft2 = FkThread()
-		ft2.start()
-print("执行完成")
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import time
+
+# 定义一个准备作为线程任务的函数
+def action(max):
+	try:
+		my_sum = 0
+		for i in range(max):
+			print(threading.current_thread().name + ' ' + str(i))
+			my_sum += 1
+		time.sleep(20)
+		return my_sum
+	except Exception as e:
+		print('1'+ str(e))
+
+try:
+	# 创建一个包含两个线程的线程池
+	with ThreadPoolExecutor(max_workers=3) as pool:
+
+		# 使用线程执行map计算
+		# 后面的元组有3个元素，
+		results = pool.map(action, [50, 100, 150, 200], timeout=10)
+		print('----------')
+		for r in results:
+			print(r)
+except Exception as e:
+	print('2'+ str(e))
