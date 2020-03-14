8c4e72143dd12993749b18589676f4b7a1ba4265
dxj728 <dxj728@163.com>
2020-03-14 10:43:30 +0800

    add:thinter鼠标移动事件绑定

--- thinter.py
+++ thinter.py
@@ -102,36 +102,70 @@
 
 ## ************************布局管理2.Grid布局管理器（表格布局）**********************
 
-# # python 2.x 使用： from Tkinter import *
-# # python 3.x 使用
-# from tkinter import *
-#
-# class APP:
-# 	def __init__(self, master):
-# 		self.master = master
-# 		self.initWidgets()
-#
-# 	def initWidgets(self):
-# 		# 创建一个输入组件
-# 		e = Entry(relief=SUNKEN, font=('Courier New', 24), width=25)
-# 		# 对该输入组件使用pack布局， 放在容器顶部
-# 		e.pack(side=TOP, pady=10)
-# 		p = Frame(self.master)
-# 		p.pack(side=TOP)
-# 		# 定义字符串元组
-# 		names = ('0', '1', '2', '3', '4', '5', '6',
-# 				 '7', '8', '9', '+', '-', '*', '/',
-# 				 '.', '=')
-# 		# 遍历字符串元组
-# 		for i in range(len(names)):
-# 			# 创建Button, 将Button放入P组件中
-# 			b = Button(p, text=names[i], font=('Verdana', 20), width=6)
-# 			b.grid(row = i // 4, column=i % 4)
-#
-# root = Tk()
-# root.title('Grid布局')
-# APP(root)
-# root.mainloop()
+# python 2.x 使用： from Tkinter import *
+# python 3.x 使用
+from tkinter import *
+
+class APP:
+	def __init__(self, master):
+		self.master = master
+		self.initWidgets()
+		self.expr=None
+
+	def initWidgets(self):
+		# 创建一个输入组件
+		self.e = Label(relief=SUNKEN, font=('Courier New', 24),\
+				  	width=25, bg='white', anchor=E)
+		# 对该输入组件使用pack布局， 放在容器顶部
+		self.e.pack(side=TOP, pady=10)
+		p = Frame(self.master)
+		p.pack(side=TOP)
+		# 定义字符串元组
+		names = ('0', '1', '2', '3', '4', '5', '6',
+				 '7', '8', '9', '+', '-', '*', '/',
+				 '.', '=')
+		# 遍历字符串元组
+		for i in range(len(names)):
+			# 创建Button, 将Button放入P组件中
+			b = Button(p, text=names[i], font=('Verdana', 20), width=6)
+			b.grid(row = i // 4, column=i % 4)
+			# 为鼠标左键的单击事件绑定事件处理办法
+			b.bind('<Button-1>', self.click)
+			# 为鼠标左键的双击事件绑定事件处理方法
+			if b['text'] == '=':
+				b.bind('<Double-1>', self.clean)
+
+	def click(self, event):
+		# 如果用户单击的是数字键或者点号
+		if (event.widget['text'] in ('0', '1', '2', '3',
+									 '4', '5', '6',
+									 '7', '8', '9', '.')):
+			self.e['text'] = self.e['text'] + event.widget['text']
+		# 如果用户单击了运算符
+		elif(event.widget['text'] in ('+', '-', '*', '/')):
+			# 如果当前表达式为NONE，则直接用show组件的内容和运算符进行连接
+			if self.expr is None:
+				self.expr = self.e['text'] + event.widget['text']
+			# 如果当前表达式不为None，则用表达式、show组件的内容和运算符进行连接
+			else:
+				self.expr = self.expr + self.e['text'] + event.widget['text']
+			self.e['text'] = ''
+		elif(event.widget['text'] == '=' and self.expr is not None):
+			self.expr = self.expr + self.e['text']
+			print(self.expr)
+			# 使用eval函数计算表达式的值
+			self.e['text'] = str(eval(self.expr))
+			self.expr = None
+
+	# 当双击‘=’时，程序清空计算结果，将表达式设为None
+	def clean(self, event):
+		self.expr = None
+		self.e['text'] = ''
+
+root = Tk()
+root.title('Grid布局-计算器')
+APP(root)
+root.mainloop()
 
 # ## ************************布局管理3.Place布局管理器（绝对布局）**********************
 #
@@ -204,38 +238,82 @@
 # ## ************************绑定简单事件**********************
 
 # python3 使用这行
-from tkinter import *
-import random
+# from tkinter import *
+# import random
+#
+# class App:
+# 	def __init__(self, master):
+# 		self.master = master
+# 		self.initWidgets()
+#
+# 	def initWidgets(self):
+# 		self.show = Label(self.master, width=30, bg='white', font=('times', 20))
+# 		self.show.pack()
+# 		bn = Button(self.master, text='单击我或双击我')
+# 		bn.pack(fill=BOTH, expand=YES)
+# 		# 为左键单击事件绑定处理方法
+# 		bn.bind('<Button-1>', self.one)
+# 		# 为左键双击事件绑定处理方法
+# 		bn.bind('<Double-1>', self.double)
+#
+# 	def one(self, event):
+# 		self.show['text'] = '左键单击：{}'.format(event.widget['text'])
+#
+# 	def double(self, event):
+# 		self.show['text'] = "左键双击，退出程序:" + event.widget['text']
+# 		print("左键双击，退出程序:" + event.widget['text'])
+#
+# 		import sys, time;
+# 		time.sleep(3)
+#
+# root = Tk()
+# root.title('简单绑定')
+# App(root)
+# root.mainloop()
 
-class App:
-	def __init__(self, master):
-		self.master = master
-		self.initWidgets()
+# # *********************************鼠标移动事件绑定练习********************
+# # python3 使用这行
+# from tkinter import *
+#
+# class App:
+# 	def __init__(self, master):
+# 		self.master = master
+# 		self.initWidgets()
+#
+# 	def initWidgets(self):
+# 		lb = Label(self.master, width=40, height=3)
+# 		lb.config(bg='lightgreen', font=('Times', 20))
+# 		# 为鼠标移动事件绑定事件处理办法
+# 		lb.bind('<Motion>', self.motion)
+# 		# 为按住左键时的鼠标移动事件绑定事件处理办法
+# 		lb.bind('<B1-Motion>', self.press_motion)
+# 		lb.pack()
+# 		self.show = Label(self.master, width=38, height=1)
+# 		self.show.config(bg='white', font=('Courier New', 20))
+# 		self.show.pack()
+#
+# 	def motion(self, event):
+# 		self.show['text'] = '鼠标移动到：(%s, %s)'% (event.x, event.y)
+# 		return
+#
+# 	def press_motion(self, event):
+# 		self.show['text'] = '按住鼠标的位置为：（%s, %s）'% (event.x, event.y)
+# 		return
+#
+# root = Tk()
+# root.title('鼠标事件')
+# App(root)
+# root.mainloop()
 
-	def initWidgets(self):
-		self.show = Label(self.master, width=30, bg='white', font=('times', 20))
-		self.show.pack()
-		bn = Button(self.master, text='单击我或双击我')
-		bn.pack(fill=BOTH, expand=YES)
-		# 为左键单击事件绑定处理方法
-		bn.bind('<Button-1>', self.one)
-		# 为左键双击事件绑定处理方法
-		bn.bind('<Double-1>', self.double)
 
-	def one(self, event):
-		self.show['text'] = '左键单击：{}'.format(event.widget['text'])
 
-	def double(self, event):
-		self.show['text'] = "左键双击，退出程序:" + event.widget['text']
-		print("左键双击，退出程序:" + event.widget['text'])
 
-		import sys, time;
-		time.sleep(3)
 
-root = Tk()
-root.title('简单绑定')
-App(root)
-root.mainloop()
+
+
+
+
+
 
 
 
