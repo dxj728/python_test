62cdadc7d1c8cbea6a5ee2148b3bf66a33d93eaa
dxj728 <dxj728@163.com>
2020-03-14 22:09:21 +0800

    add:ttk组件(下拉列表)

--- thinter.py
+++ thinter.py
@@ -100,72 +100,72 @@
 # root.mainloop()
 
 
-## ************************布局管理2.Grid布局管理器（表格布局）**********************
-
-# python 2.x 使用： from Tkinter import *
-# python 3.x 使用
-from tkinter import *
-
-class APP:
-	def __init__(self, master):
-		self.master = master
-		self.initWidgets()
-		self.expr=None
-
-	def initWidgets(self):
-		# 创建一个输入组件
-		self.e = Label(relief=SUNKEN, font=('Courier New', 24),\
-				  	width=25, bg='white', anchor=E)
-		# 对该输入组件使用pack布局， 放在容器顶部
-		self.e.pack(side=TOP, pady=10)
-		p = Frame(self.master)
-		p.pack(side=TOP)
-		# 定义字符串元组
-		names = ('0', '1', '2', '3', '4', '5', '6',
-				 '7', '8', '9', '+', '-', '*', '/',
-				 '.', '=')
-		# 遍历字符串元组
-		for i in range(len(names)):
-			# 创建Button, 将Button放入P组件中
-			b = Button(p, text=names[i], font=('Verdana', 20), width=6)
-			b.grid(row = i // 4, column=i % 4)
-			# 为鼠标左键的单击事件绑定事件处理办法
-			b.bind('<Button-1>', self.click)
-			# 为鼠标左键的双击事件绑定事件处理方法
-			if b['text'] == '=':
-				b.bind('<Double-1>', self.clean)
-
-	def click(self, event):
-		# 如果用户单击的是数字键或者点号
-		if (event.widget['text'] in ('0', '1', '2', '3',
-									 '4', '5', '6',
-									 '7', '8', '9', '.')):
-			self.e['text'] = self.e['text'] + event.widget['text']
-		# 如果用户单击了运算符
-		elif(event.widget['text'] in ('+', '-', '*', '/')):
-			# 如果当前表达式为NONE，则直接用show组件的内容和运算符进行连接
-			if self.expr is None:
-				self.expr = self.e['text'] + event.widget['text']
-			# 如果当前表达式不为None，则用表达式、show组件的内容和运算符进行连接
-			else:
-				self.expr = self.expr + self.e['text'] + event.widget['text']
-			self.e['text'] = ''
-		elif(event.widget['text'] == '=' and self.expr is not None):
-			self.expr = self.expr + self.e['text']
-			print(self.expr)
-			# 使用eval函数计算表达式的值
-			self.e['text'] = str(eval(self.expr))
-			self.expr = None
-
-	# 当双击‘=’时，程序清空计算结果，将表达式设为None
-	def clean(self, event):
-		self.expr = None
-		self.e['text'] = ''
-
-root = Tk()
-root.title('Grid布局-计算器')
-APP(root)
-root.mainloop()
+# ## ************************布局管理2.Grid布局管理器练习-计算器（表格布局）**********************
+#
+# # python 2.x 使用： from Tkinter import *
+# # python 3.x 使用
+# from tkinter import *
+#
+# class APP:
+# 	def __init__(self, master):
+# 		self.master = master
+# 		self.initWidgets()
+# 		self.expr=None
+#
+# 	def initWidgets(self):
+# 		# 创建一个输入组件
+# 		self.e = Label(relief=SUNKEN, font=('Courier New', 24),\
+# 				  	width=25, bg='white', anchor=E)
+# 		# 对该输入组件使用pack布局， 放在容器顶部
+# 		self.e.pack(side=TOP, pady=10)
+# 		p = Frame(self.master)
+# 		p.pack(side=TOP)
+# 		# 定义字符串元组
+# 		names = ('0', '1', '2', '3', '4', '5', '6',
+# 				 '7', '8', '9', '+', '-', '*', '/',
+# 				 '.', '=')
+# 		# 遍历字符串元组
+# 		for i in range(len(names)):
+# 			# 创建Button, 将Button放入P组件中
+# 			b = Button(p, text=names[i], font=('Verdana', 20), width=6)
+# 			b.grid(row = i // 4, column=i % 4)
+# 			# 为鼠标左键的单击事件绑定事件处理办法
+# 			b.bind('<Button-1>', self.click)
+# 			# 为鼠标左键的双击事件绑定事件处理方法
+# 			if b['text'] == '=':
+# 				b.bind('<Double-1>', self.clean)
+#
+# 	def click(self, event):
+# 		# 如果用户单击的是数字键或者点号
+# 		if (event.widget['text'] in ('0', '1', '2', '3',
+# 									 '4', '5', '6',
+# 									 '7', '8', '9', '.')):
+# 			self.e['text'] = self.e['text'] + event.widget['text']
+# 		# 如果用户单击了运算符
+# 		elif(event.widget['text'] in ('+', '-', '*', '/')):
+# 			# 如果当前表达式为NONE，则直接用show组件的内容和运算符进行连接
+# 			if self.expr is None:
+# 				self.expr = self.e['text'] + event.widget['text']
+# 			# 如果当前表达式不为None，则用表达式、show组件的内容和运算符进行连接
+# 			else:
+# 				self.expr = self.expr + self.e['text'] + event.widget['text']
+# 			self.e['text'] = ''
+# 		elif(event.widget['text'] == '=' and self.expr is not None):
+# 			self.expr = self.expr + self.e['text']
+# 			print(self.expr)
+# 			# 使用eval函数计算表达式的值
+# 			self.e['text'] = str(eval(self.expr))
+# 			self.expr = None
+#
+# 	# 当双击‘=’时，程序清空计算结果，将表达式设为None
+# 	def clean(self, event):
+# 		self.expr = None
+# 		self.e['text'] = ''
+#
+# root = Tk()
+# root.title('Grid布局-计算器')
+# APP(root)
+# root.mainloop()
 
 # ## ************************布局管理3.Place布局管理器（绝对布局）**********************
 #
@@ -305,6 +305,34 @@ root.mainloop()
 # App(root)
 # root.mainloop()
 
+## ******************************* ttk组件(下拉列表) ***************************
+
+# python3 使用这行
+from tkinter import *
+from tkinter import ttk
+
+class App:
+	def __init__(self, master):
+		self.master = master
+		self.initWidgets()
+
+	def initWidgets(self):
+		# ttk使用了Combobox取代了Listbox
+		cb = ttk.Combobox(self.master, font=24)
+		# 为Combobox设置列表项
+		cb['values'] = ('python', 'swift', 'c++')
+		cb.pack(side=LEFT, fill=X, expand=YES)
+		f = ttk.Frame(self.master)
+		f.pack(side=RIGHT, fill=BOTH, expand=YES)
+		lab = ttk.Label(self.master, text='我的标签', font=24)
+		lab.pack(side=TOP, fill=BOTH, expand=YES)
+		bn = ttk.Button(self.master, text='我的按钮')
+		bn.pack()
+
+root = Tk()
+root.title('简单事件处理')
+App(root)
+root.mainloop()
 
 
 
