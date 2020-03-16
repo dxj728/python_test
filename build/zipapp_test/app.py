ea9931d17d1cc957216233d38849b9262e39a72b
dxj728 <dxj728@163.com>
2020-03-16 23:59:24 +0800

    add:打包发布学习

---
+++ build/zipapp_test/app.py
@@ -0,0 +1,13 @@
+# --coding:utf-8--
+# File: app.py
+# Author: dxj728
+# Time: 2020年03月16日21时
+# 说明：zipapp模块练习
+
+from say_hello import *
+
+def main():
+	print('程序开始执行！')
+	print(say_hello('孙悟空'))
+
+
---
+++ build/zipapp_test/say_hello.py
@@ -0,0 +1,10 @@
+# --coding:utf-8--
+# File: say_hello.py
+# Author: dxj728
+# Time: 2020年03月16日21时
+# 说明：zipapp打包模块练习
+
+def say_hello(name):
+	return name + '你好！'
+
+
