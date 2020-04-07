ea72f53da2464225f5f6aefa5122eaf3863c2c32
dxj728 <dxj728@163.com>
2020-04-08 00:38:30 +0800

    add:unittest单元测试学习2

--- module/unittest_mod.py
+++ module/unittest_mod.py
@@ -3,7 +3,14 @@
 # Author: dxj728
 # Time: 2020年04月06日23时
 # 说明：单元测试学习(unittest模块)
-
+'''
+	基础概念：
+		测试用例： 测试用例类(下一概念)中以test_开头的方法
+		测试用例类：单个的测试单元，unittest提供了一个TestCase基类用于创建测试用例类
+		测试包： 用于组合多个测试用例，测试包也可以嵌套测试包
+		测试运行器： 负责组织、运行测试用例，并向用户呈现测试结果
+		测试固件： 代表执行一个或多个测试用例所需的准备工作，以及相关联的准备操作（如创建临时数据库、创建目录、开启服务器进程等）
+'''
 import unittest
 
 # -----------------------单测例测试-------------------------------
@@ -43,14 +50,41 @@ class TestCrazyitModule(unittest.TestCase):
 		with self.assertRaises(ValueError):
 			two_equation(4, 2, 3)
 
-	#（可选） 每个测试用例执行前(test开头方法)执行该方法进行初始化
+	'''
+		跳过测试用例方式一: 使用skip装饰器来跳过测试用例
+			@unittest.skip(reason)
+			@unittest.skipIf(condition, reason)
+			@unittest.skipUnless(condition, reason)
+	'''
+	@unittest.skip('临时跳过')
+	def test_add(self):
+		self.assertEqual(one_equation(5, 9), 1.8)
+
+	'''
+		跳过测试用例方式二: 使用TestCase的skipTest()方法来跳过测试用例
+	'''
+	def test_add(self):
+		self.skipTest('临时跳过')
+		self.assertEqual(one_equation(5, 9), 1.8)
+
+
+	# （可选） 每个测试用例执行前(test开头方法)执行该方法进行初始化
 	def setUp(self):
 		print('\n====执行setUp模拟初始化固件====')
 
-	#（可选） 每个测试用例执行后(test开头方法)执行该方法进行销毁
+	# （可选） 每个测试用例执行后(test开头方法)执行该方法进行销毁
 	def tearDown(self):
 		print('\n====调用tearDown模拟销毁固件====')
 
+	# （可选）在该类的所有测试用例执行之前执行一次该方法进行初始化
+	@classmethod
+	def setUpClass(cls):
+		print('\n====执行setUpClass在类级别模拟初始化测试固件=====')
+
+	# （可选）在该类的所有测试用例执行之后执行一次该方法进行销毁
+	@classmethod
+	def tearDownClass(cls):
+		print('\n====执行tearDownClass在类级别模拟销毁测试固件=====')
 
 ''' 
 	第一种执行运行方式：main语句执行
