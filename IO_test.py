4cf414cf1a183954b1dabf53881b1c52ee6b99d8
dxj728 <dxj728@163.com>
2021-01-14 23:19:16 +0800

    add:IO简单学习1

--- IO_test.py
+++ IO_test.py
@@ -6,10 +6,36 @@
 
 import os
 
-"""获取用户输入"""
-msg = input("请输入你的值：")      # python3中使用该方法获取用户输入，等同于python2中raw_input()，返回字符串
-print(msg)          # hi
-print(type(msg))    # <class 'str'>
+"""input(): python3中使用该方法获取用户输入,等同于python2中raw_input()"""
+# msg = input("请输入你的值：")      # 返回用户输入的内容，均为字符串类型
+# 示例：
+# msg1 = input().strip()              # 单行输入，返回字符串
+# msg_list2 = list(map(int, input().strip().split()))      # 单行间隔输入(空格)，返回列表(当前强转为整型)
+# msg_list3 = [int(input().strip()) for _ in range(6)]          # 多行输入，返回列表(元素强转为整型)
+# msg_list4 = [list(map(int, input().strip().split())) for _ in range(6)]      # 多行多间隔输入，返回嵌套列表(元素强转为整型)
+
+# print('\n'.join(map(str, msg_list4)))    # 返回结果表达
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
