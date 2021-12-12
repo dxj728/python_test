cab898da11c2897683cf207c06c2690188657bb4
dxj728 <dxj728@163.com>
2021-12-12 23:11:31 +0800

    add:leecode

---
+++ problem_test/leecode.py
@@ -0,0 +1,39 @@
+# --coding:utf-8--
+# File: leecode.py
+# Author: dxj728
+# Time: 2021年12月12日10时
+# 说明：；力扣(leecode)做题
+
+
+# 2021.12.12
+'''7. 整数反转'''
+# def reverse(x: int) -> int:
+#
+#     str_x = str(x)
+#     str_x = str_x[::-1] if '-' not in str_x else '-' + str_x[:0:-1]
+#     ret = int(str_x)
+#     if ret < -2147483648 or ret > 2147483647:
+#         ret = 0
+#     return ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
