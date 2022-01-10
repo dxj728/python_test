5e4e9e6b1502f52a98e77ddf0b34647da7a5d78f
dxj728 <dxj728@163.com>
2022-01-10 23:41:37 +0800

    add:do exercise

--- problem_test/coding_interviews.py
+++ problem_test/coding_interviews.py
@@ -92,13 +92,13 @@ class Solution03:
 
 
 '''剑指 Offer 53 - I. 在排序数组中查找数字 I'''
-class Solution53:
+class Solution53i:
     def search(self, nums: List[int], target: int) -> int:
         return nums.count(target)
 
 
 '''剑指 Offer 53 - II. 0～n-1中缺失的数字'''
-class Solution53:
+class Solution53ii:
     def missingNumber(self, nums: List[int]) -> int:
         for i in range(0, len(nums)):
             if i != len(nums) -1:
@@ -119,24 +119,64 @@ class Solution04:
         return False
 
 
+'''剑指 Offer 11. 旋转数组的最小数字'''
+class Solution11:
+    def minArray(self, numbers: List[int]) -> int:
+        return min(numbers)
 
 
+'''剑指 Offer 50. 第一个只出现一次的字符'''
+class Solution50:
+    def firstUniqChar(self, s: str) -> str:
+        if len(s) == 0:
+            return " "
+        ret_dict = {}
+        for sing in s:
+            if sing not in ret_dict:
+                ret_dict[sing] = 1
+            else:
+                ret_dict[sing] = ret_dict[sing] + 1
+        if 1 not in ret_dict.values():
+            return " "
+        for k, v in ret_dict.items():
+            if v == 1:
+                return k
 
 
+'''剑指 Offer 10- I. 斐波那契数列'''
+class Solution10i:
+    def fib(self, n: int) -> int:
+        a, b = 0, 1
+        for i in range(n):
+            a,b = b, (a+b)%1000000007
+        else:
+            return a
 
 
+'''剑指 Offer 10- II. 青蛙跳台阶问题'''
+class Solution10ii:
+    def numWays(self, n: int) -> int:
+        a, b = 1, 1
+        for i in range(n):
+            a,b = b, (a+b)%1000000007
+        else:
+            return a
 
 
-
-
-
-
-
-
-
-
-
-
+'''剑指 Offer 63. 股票的最大利润'''
+class Solution63:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0 or len(prices) == 1:
+            return 0
+        ret_list = []
+        end = len(prices)
+        for i in range(1, end):
+            max_value = max(prices[i:end])
+            min_value = min(prices[0:i])
+            tmp = max_value - min_value
+            ret_list.append(tmp)
+        ret = 0 if max(ret_list) <= 0 else max(ret_list)
+        return ret
 
 
 
